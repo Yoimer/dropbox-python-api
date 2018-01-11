5d36bac47d520c0f529898cda52b3d636381339c
Yoimer <ydrf19@gmail.com>
2018-01-11 19:32:06 -0400

    read content from an specific folder

--- main.py
+++ main.py
@@ -2,5 +2,10 @@ import dropbox
 #dbx = dropbox.Dropbox('YOUR_ACCESS_TOKEN')
 dbx = dropbox.Dropbox('iTG45yQvU6sAAAAAAAAFZ2miQx9MlrEpJsXYb6Lf44YLfsoLkfslAE9c6kDpQzPV')
 dbx.users_get_current_account()
+
+# empty means root directoty (/root)
 for entry in dbx.files_list_folder('').entries:
+
+# this is an especific folder
+for entry in dbx.files_list_folder('/Apps/yoimer1').entries:
     print(entry.name)
