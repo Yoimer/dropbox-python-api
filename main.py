07109cab9b8d0642bc896607b65b854108717c8d
Yoimer <ydrf19@gmail.com>
2018-01-11 21:49:58 -0400

    fixed retyping error on main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ dbx = dropbox.Dropbox('iTG45yQvU6sAAAAAAAAFZ2miQx9MlrEpJsXYb6Lf44YLfsoLkfslAE9c6
 dbx.users_get_current_account()
 
 # empty means root directoty (/root)
-for entry in dbx.files_list_folder('').entries:
+#for entry in dbx.files_list_folder('').entries:
 
 # this is an especific folder
 for entry in dbx.files_list_folder('/Apps/yoimer1').entries:
